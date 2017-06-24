498c256f94705b04b8a22b8c39f7d33895e0b952
brobird <netassadove@gmail.com>
2017-06-24 15:38:34 +0800

    initial main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,8 @@
-module.exports = function main() {
-  // Write your cade here
+module.exports = function main(num) {
+  // Write your code here
+	var sum = 0;
+	for(var i=0;i<num.length;i++){
+		sum += num[i];
+	}
+	return sum;
 };
