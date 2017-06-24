78faa11c1b9fa6f0b0267445d5c9db9779552015
brobird <netassadove@gmail.com>
2017-06-24 15:44:04 +0800

    update main.js

--- main/main.js
+++ main/main.js
@@ -2,7 +2,7 @@ module.exports = function main(num) {
   // Write your code here
 	var sum = 0;
 	for(var i=0;i<num.length;i++){
-		sum += Number(num[i];)
+		sum += Number(num[i]);
 	}
 	return sum;
 };
